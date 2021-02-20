f7b41e4b33292e7917d848730f1526586ca6a8fb
Jonathan Hao <haojonat@amazon.com>
2021-02-20 13:54:54 -0500

    adding a test banner

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import classnames from 'classnames';
+
+const Banner = () => {
+  const classNames = classnames('banner');
+
+  return (
+    <div className={classNames}>
+      I AM TECHNICALLY A BANNER
+    </div>
+  )
+}
+
+export default Banner;
