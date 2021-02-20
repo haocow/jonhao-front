2555ec31fe4d5d1e011212c76f59eb1ebc0db22f
Jonathan Hao <haojonat@amazon.com>
2021-02-20 14:07:16 -0500

    use Router

--- src/index.jsx
+++ src/index.jsx
@@ -4,11 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Banner from './components/Banner/Banner';
-
 ReactDOM.render(
   <React.StrictMode>
-    <Banner />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFoundPage = () => {
+  return (
+    <div>
+      NOT FOUND!
+    </div>
+  );
+}
+
+export default NotFoundPage;
